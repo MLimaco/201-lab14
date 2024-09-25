094cfeab90260573368a0880e48618b98a81e58f
MICHAEL LIMACO <limaco.michael@gmail.com>
2024-09-24 23:17:50 -0500

    lab 14 update

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ AppState.prototype.instantiateProducts = function () {
 
   for (let i = 0; i < productNames.length; i++) {
     if (productNames[i] === 'sweep') {
-      this.allProducts.push(new Product(productNames[i], 'png'))
+      this.allProducts.push(new Product(productNames[i], 'png')) 
     } else {
       this.allProducts.push(new Product(productNames[i]))
     }
@@ -20,13 +20,48 @@ AppState.prototype.instantiateProducts = function () {
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
+  let convertirValue = JSON.stringify(this.allProducts);
+  localStorage.setItem('allProducts', convertirValue);
 }
 
 AppState.prototype.loadItems = function () {
 
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
+  let respuestaProductos = localStorage.getItem('allProducts');
+  let parseRespuesta = JSON.parse(respuestaProductos);
+  console.log(parseRespuesta);
 
-  this.instantiateProducts();
+  if (respuestaProductos) {
+    this.allProducts = parseRespuesta;
+    // for(let i=0; i<parsedProducts.length; i++){
+    //   if(parsedProducts[i].name=== 'sweep'){
+    //     let reconstructedSweepProduct=new Product(parsedProducts[i].name, 'png');
+    //     reconstructedSweepProduct.timesShown=parsedProducts[i].timesShown;
+    //     reconstructedSweepProduct.timesClicked=parsedProducts[i].timesClicked;
+    //     this.allProducts.push(reconstructedSweepProduct);
+    //   }else{
+    //     let reconstructedProduct=new Product(parsedProducts[i].name);
+    //     reconstructedProduct.timesShown=parsedProducts[i].timesShown;
+    //     reconstructedProduct.timesClicked=parsedProducts[i].timesClicked;
+    //     this.allProducts.push(reconstructedProduct);
+    //   }
+    // }
+  } else {
+    this.instantiateProducts();
+  }
+  console.log(this.allProducts[0].timesClicked);
+  //if(respuestaProductos){
+  //   for(let i=0;i<parseRespuesta.length;i++){
+  //     if( parseRespuesta[i].name==='sweep'){
+
+  //     }else{
+
+  //     }
+  //   }
+  //}else{
+  //   this.instantiateProducts();
+  //}
+
 
 }
 
--- js/chart.js
+++ js/chart.js
@@ -11,6 +11,65 @@ let canvasElem = document.getElementById('chart')
  *
  */
 function renderChart() {
+    let state=new AppState();
+    state.loadItems();
+
+    let productNames=[];
+    let productClicks=[];
+    let productViews=[];
+
+    for(let i=0;i<state.allProducts.length;i++){
+        productNames.push(state.allProducts[i].name);
+        productClicks.push(state.allProducts[i].timesClicked);
+        productViews.push(state.allProducts[i].timesShown);
+    }
+    Chart.defaults.font.size=20;
+    Chart.defaults.font.weight='bold';
+
+    let chartObj={
+        type:'bar',
+        data:{
+            labels:productNames,
+            datasets:[{
+                label:'# of votes',
+                data:productClicks,
+                borderWidth:5,
+                backgroundColor:['#ff006e'],
+                borderColor:['#ff006e']
+            },
+            {
+                label:'# of views',
+                data:productViews,
+                borderWidth:5,
+                backgroundColor:['#8338ec'],
+                borderColor:['#8338ec']
+            }]
+        },
+        option:{
+            indexAxis:'y',
+            scales:{
+                y:{
+                    beginAtZero:true,
+                    ticks:{color:'black'}
+                },
+                x:{
+                    ticks:{color:'black'}
+                }
+            },
+            plugins:{
+                legend:{
+                    labels:{
+                        color:'black',
+                        padding:30,
+                        font:{
+                            size:16
+                        }
+                    },
+                }
+            }
+        }
+    }
+    new Chart(canvasElem,chartObj);
 }
 
 renderChart();
